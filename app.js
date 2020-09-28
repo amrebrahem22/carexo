73620eb1f2814557ded034ea54864fc6806bf68d
amrebrahem226 <amrebrahem226@gmail.com>
2020-09-28 19:18:46 +0200

    add error handlers and validation to signup

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require('express-validator');
 require("dotenv").config();
 const userRouter = require('./routes/user');
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.DATABASE, {
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // Routes
 app.use('/api', userRouter);
